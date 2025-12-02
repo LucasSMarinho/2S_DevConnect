7e2d981f16daef8f635cca7b8fa961ad94dfd1a3
Lucas MArinho <lucas.marinho.ceu@gmail.com>
2025-12-02 16:42:17 -0300

    Subindo primeira parte do projeto

---
+++ MvcDevConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcDevConnect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
